783692e2d11bf92bc15ae8cb5efc476edee38fe5
Calvin Cestari <calvin@calvincestari.com>
2014-04-05 23:08:04 -0700

    Remove background and gradient

--- PaymentKit/PKView.m
+++ PaymentKit/PKView.m
@@ -80,10 +80,6 @@ static NSString *const kPKOldLocalizedStringsTableName = @"STPaymentLocalizable"
     self.textColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
 }
 
-    UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:self.bounds];
-    backgroundImageView.image = [[UIImage imageNamed:@"textfield"]
-            resizableImageWithCapInsets:UIEdgeInsetsMake(0, 8, 0, 8)];
-    [self addSubview:backgroundImageView];
 
     self.innerView = [[UIView alloc] initWithFrame:CGRectMake(40, 12, self.frame.size.width - 40, 20)];
     self.innerView.clipsToBounds = YES;
@@ -95,15 +91,7 @@ static NSString *const kPKOldLocalizedStringsTableName = @"STPaymentLocalizable"
 
     [self.innerView addSubview:self.cardNumberField];
 
-    UIImageView *gradientImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 12, 34)];
-    gradientImageView.image = [UIImage imageNamed:@"gradient"];
-    [self.innerView addSubview:gradientImageView];
 
-    self.opaqueOverGradientView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 9, 34)];
-    self.opaqueOverGradientView.backgroundColor = [UIColor colorWithRed:0.9686 green:0.9686
-                                                                   blue:0.9686 alpha:1.0000];
-    self.opaqueOverGradientView.alpha = 0.0;
-    [self.innerView addSubview:self.opaqueOverGradientView];
 
     [self addSubview:self.innerView];
     [self addSubview:self.placeholderView];
